64305a47dcfa037e789796e024fb63c917da56aa
Dima <dmiorlov@uat.edu>
2025-04-11 14:23:00 -0500

    sfgsdd

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const img = document.createElement('img');
-img.src = '/mysteryBox.png'; // or a full URL if hosted elsewhere
+img.src = 'public/mysteryBox.png'; // or a full URL if hosted elsewhere
 img.alt = 'Mystery Box';
 img.style.cursor = 'grab';
 img.style.width = '100px';
